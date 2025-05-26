2f4951be281bac12c8811dc6e6ca0f9a7b8ff78a
AndriantsoaRts <andriantsoa024@gmail.com>
2025-05-26 11:21:58 +0300

    mise en place base front

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,28 @@
-import './bootstrap.js';
+import "./bootstrap.js";
 /*
  * Welcome to your app's main JavaScript file!
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+import "./styles/app.css";
+import "./styles/dashboard.css";
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+document.addEventListener("turbo:load", function () {
+  const profileIcon = document.querySelector(".profile_icon");
+  const dropdownMenu = document.querySelector(".dropdown_menu ul");
+  if (!profileIcon || !dropdownMenu) {
+    return;
+  }
+
+  profileIcon.addEventListener("click", function (e) {
+    e.preventDefault();
+    dropdownMenu.classList.toggle("show");
+  });
+
+  document.addEventListener("click", function (e) {
+    if (!e.target.closest(".profile")) {
+      dropdownMenu.classList.remove("show");
+    }
+  });
+});
